c4573a948150cad7b441632b86dbec61c0b7a4b0
Arturo G. Bruno <arturogbruno@gmail.com>
2020-06-25 14:56:28 +0200

    Refactor app.js

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-const input = process.argv[2].split("\n");
-console.log(input);
+const init = input => {
+    const gridInput = input[0];
+    const gridSize = checkGridSize(gridInput);
 
-const gridInput = input[0];
-const gridSize = gridInput.split(" ");
-if(gridSize.some(x => x >50 || x < 2)) {
-    const invalidGridMsg = "Invalid grid size. Size should be between 2x2 and 50x50.";
-    throw invalidGridMsg;
-}
-
-console.log(gridSize);
-
-exports.grid = {
-    sizeX: +gridSize[0],
-    sizeY: +gridSize[1],
-    scents: []
-}
-
-const robotsInput = input.slice(1);
-if(robotsInput.length % 2 !== 0) {
-    const invalidInfoMsg = "Info provided for robots is incomplete.";
-    throw invalidInfoMsg;
-}
-
-const robotsInfo = robotsInput.reduce((ac, cv, idx, arr) => {
-    if(idx % 2 === 0) {
-        ac.push(arr.slice(idx, idx + 2).join(" ").split(" "));
+    exports.grid = {
+        sizeX: +gridSize[0],
+        sizeY: +gridSize[1],
+        scents: []
     }
-    return ac;
-}, []);
 
-console.log(robotsInfo);
+    const robotsInput = input.slice(1);
+    const checkedRobotsInput = checkCompleteInfo(robotsInput);
 
-const Robot = require("./robot");
+    const robotsInfo = checkedRobotsInput.reduce((ac, cv, idx, arr) => {
+        if(idx % 2 === 0) {
+            ac.push(arr.slice(idx, idx + 2).join(" ").split(" "));
+        }
+        return ac;
+    }, []);
 
-let result = [];
+    const Robot = require("./robot");
+    let result = [];
 
-robotsInfo.forEach((robotInfo, idx) => {
-    const robot = new Robot(robotInfo, idx);
-    result.push(robot.operateRobot());
-});
+    robotsInfo.forEach((robotInfo, idx) => {
+        const robot = new Robot(robotInfo, idx);
+        result.push(robot.operateRobot());
+    });
+}
 
-console.log(result);
+const checkGridSize = gridInput => {
+    const gridSize = gridInput.split(" ");
+    if(gridSize.some(x => x > 50 || x < 2)) {
+        const invalidGridMsg = "Invalid grid size. Size should be between 2x2 and 50x50.";
+        throw invalidGridMsg;
+    }
+    return gridSize;
+}
+
+const checkCompleteInfo = robotsInput => {
+    if(robotsInput.length % 2 !== 0) {
+        const invalidInfoMsg = "Info provided for robots is incomplete.";
+        throw invalidInfoMsg;
+    }
+    return robotsInput;
+}
+
+const input = process.argv[2].split("\n");
+init(input);
